94fe81f6b2ec9825576c5dd8c0422e8ce0c74299
Tomas Petricek <tpetricek@gmail.com>
2022-08-10 20:01:55 +0200

    Enable saving ortho map.

--- src/laser_scanner_postproc/show_clouds.py
+++ src/laser_scanner_postproc/show_clouds.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
 from .clouds import merge_clouds
-from .visualization import show_cloud
+from .visualization import show_cloud, show_ortho
 from argparse import ArgumentParser
 import yaml
 
@@ -10,6 +10,9 @@ def main():
     parser.add_argument('--box', '-b', type=str, default='[[-.inf, -.inf, -.inf], [.inf, .inf, .inf]]')
     parser.add_argument('--range', '-r', type=str, default='[0.0, .inf]')
     parser.add_argument('--grid', '-g', type=str, default='[0.0, "first"]')
+    parser.add_argument('--ortho', type=str, default=None)
+    parser.add_argument('--ortho-axes', type=str, default='xy')
+    parser.add_argument('--ortho-grid', type=float, default=10.0)
     parser.add_argument('input', nargs='+', type=str)
     args = parser.parse_args()
     args.box = yaml.safe_load(args.box)
@@ -18,6 +21,11 @@ def main():
 
     cloud = merge_clouds(args.input, args.box, args.range, args.grid)
     show_cloud(cloud)
+    if args.ortho is not None:
+        fig = show_ortho(cloud, axes=args.ortho_axes, grid=args.ortho_grid)
+        if args.ortho:
+            fig.savefig(args.ortho, bbox_inches='tight', dpi=300)
+            print('Ortho image saved to %s.' % args.ortho)
 
 
 if __name__ == '__main__':
--- src/laser_scanner_postproc/visualization.py
+++ src/laser_scanner_postproc/visualization.py
@@ -1,4 +1,8 @@
 from __future__ import absolute_import, division, print_function
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.ticker
+import matplotlib.pyplot as plt
 import numpy as np
 from numpy.lib.recfunctions import structured_to_unstructured
 from PIL import Image
@@ -42,3 +46,28 @@ def show_rgb_image(cloud, rotate=0):
     if rotate == 90.0:
         rgb = rgb.transpose((1, 0, 2))[::-1]
     Image.fromarray(rgb).show()
+
+
+def show_ortho(cloud, axes='xy', grid=10.0):
+    cloud = cloud.ravel()
+    x = cloud[axes[0]]
+    y = cloud[axes[1]]
+    rgb = structured_to_unstructured(cloud[['r', 'g', 'b']]) / 255.
+    xlim = [np.floor(x.min() / grid) * grid, np.ceil(x.max() / grid) * grid]
+    ylim = [np.floor(y.min() / grid) * grid, np.ceil(y.max() / grid) * grid]
+    nx = round((xlim[1] - xlim[0]) / grid)
+    ny = round((ylim[1] - ylim[0]) / grid)
+    xticks = np.linspace(xlim[0], xlim[1], nx + 1)
+    yticks = np.linspace(ylim[0], ylim[1], ny + 1)
+    fig, ax = plt.subplots(1, 1, figsize=(nx, ny))
+    ax.scatter(x, y, s=1, c=rgb, marker=',', lw=0, alpha=0.5)
+    ax.grid()
+    ax.axis('equal')
+    ax.set_xlabel('%s [m]' % axes[0])
+    ax.set_ylabel('%s [m]' % axes[1])
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.set_xticks(xticks)
+    ax.set_yticks(yticks)
+    plt.tight_layout()
+    return fig
